8e9890a94f40e1782c361b793816095b1b13a6a8
manuelmenama <manuel.mena090589@gmail.com>
2023-03-14 15:06:46 +0100

    added control in title and pageNumber

--- src/org/lessons/java/library/Book.java
+++ src/org/lessons/java/library/Book.java
@@ -9,8 +9,70 @@ public class Book {
 
     //COSTRUTTORI
 
+    public Book(String title, int pageNumber, String author, String editor) throws IllegalArgumentException {
+
+        isEmptyTitle(title);
+        this.title = title;
+        positiveNumberOfPage(pageNumber);
+        this.pageNumber = pageNumber;
+        this.author = author;
+        this.editor = editor;
+    }
+
     //SETTER E GETTER
 
-    //METODI
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) throws IllegalArgumentException{
+        isEmptyTitle(title);
+        this.title = title;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) throws IllegalArgumentException{
+        positiveNumberOfPage(pageNumber);
+        this.pageNumber = pageNumber;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    //METODI
+    private void isEmptyTitle(String title) throws IllegalArgumentException{
+        if (title.isEmpty()){
+            throw new IllegalArgumentException("Il campo titolo non può essere vuoto.");
+        }
+    }
+
+    private void positiveNumberOfPage(int pageNumber) throws IllegalArgumentException{
+        if (pageNumber <= 0) {
+            throw new IllegalArgumentException("Il numero di pagine deve essere superiore di 0.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Titolo: " + title + "\n" +
+                "Numero Pagine: " + pageNumber + "\n" +
+                "Autore: " + author + "\n" +
+                "Editore: " + editor + "\n";
+    }
 }
--- src/org/lessons/java/library/Summary.java
+++ src/org/lessons/java/library/Summary.java
@@ -2,6 +2,16 @@ package org.lessons.java.library;
 
 public class Summary {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Book newBook = new Book("Melanzane alla Parmigiana", 100, "Mimmo", "Bompiani");
+
+        newBook.setTitle("Melanzanine");
+
+        newBook.setPageNumber(200);
+
+        System.out.println(newBook);
+
+
+
     }
 }
