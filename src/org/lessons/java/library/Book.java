b31c8906635da0da3e745d9048f8e63e7a013fc6
manuelmenama <manuel.mena090589@gmail.com>
2023-03-14 14:20:22 +0100

    first commit

---
+++ src/org/lessons/java/library/Book.java
@@ -0,0 +1,16 @@
+package org.lessons.java.library;
+
+public class Book {
+    //ATTRIBUTI
+    private String title;
+    private int pageNumber;
+    private String author;
+    private String editor;
+
+    //COSTRUTTORI
+
+    //SETTER E GETTER
+
+    //METODI
+
+}
---
+++ src/org/lessons/java/library/Summary.java
@@ -0,0 +1,7 @@
+package org.lessons.java.library;
+
+public class Summary {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
